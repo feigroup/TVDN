c91718e7d06707bfe45b3b3ccbf74bc0968303dc
JINhuaqing <kevinjin0423@gmail.com>
2020-10-28 11:59:43 +0800

    Add brain plot for canonical networks And some U for default network

--- Rcode/saveNii.R
+++ Rcode/saveNii.R
@@ -1,8 +1,8 @@
 library(magrittr)
 source("utils.R")
 
-library(oro.nifti)
 library(RNifti)
+library(oro.nifti)
 library(neurobase)
 
 Rlibpath = "../necessary files"
@@ -18,4 +18,53 @@ for (em in 1:ncol(weightedU)){
     outPut <- innOut(weightedU[, em],region_list, AAL)
     writeNifti(outPut, file = paste0("./brainsPlot/", 'fMRI', idx, "_", em, '.nii'))
 }
+
+
+
+# save the nii for canonical networks
+fcR = read.csv('../necessary files/AALICA.csv')
+fcR = fcR[1:90, ] # 90 x 7
+nams <- names(fcR)
+for (i in 1:7){
+    name <- nams[i]
+    outPut <- innOut(fcR[, i], region_list, AAL)
+    writeNifti(outPut, file=paste0("./brainsPlot/", name, ".nii"))
+}
+
+
+# The correlation between wU of selected data and fcR 
+idxs <- c(5, 15, 58, 70, 149, 230)
+for (idx in idxs){
+    filName <- paste0("../realdata/midRess/fMRI", idx, "wU.txt")
+    saveName <- paste0("../realdata/midRess/fMRI", idx, "corrMax.txt")
+    weightedU <- read.table(filName)
+    corrs <- corF.fMRI(weightedU, fcR)
+    midxs <- apply(corrs, 1, which.max)
+    mnames <- nams[midxs]
+    write.table(rbind(midxs, mnames), saveName)
+}
+
+
+# The nii file of U with largest correlation with Default network
+name <- "Default"
+dat <- read.table("../results/max_Default_U.txt")
+names(dat) <- NULL
+dat <- as.matrix(dat)
+dat.corr <- read.table("../results/max_Default_U_corrV.txt")
+names(dat.corr) <- NULL
+dat.corr <- unlist(dat.corr)
+
+
+mU <- colMeans(dat)
+outPut <- innOut(mU, region_list, AAL)
+writeNifti(outPut, file=paste0("./brainsPlot/", "max_meanU", name, ".nii"))
+
+minU <- dat[which.min(dat.corr), ]
+maxU <- dat[which.max(dat.corr), ]
+
+outPut <- innOut(minU, region_list, AAL)
+writeNifti(outPut, file=paste0("./brainsPlot/", "max_minU", name, ".nii"))
+outPut <- innOut(maxU, region_list, AAL)
+writeNifti(outPut, file=paste0("./brainsPlot/", "max_maxU", name, ".nii"))
+
 
